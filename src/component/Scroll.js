591121e2d77833eca3a5f25ef99441c33ef7f6dd
goto florian <gotoflorian.pro@gmail.com>
2018-03-04 00:14:24 +0100

    add destructuring on state object

--- src/component/Scroll.js
+++ src/component/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = ({ children }) => {
     return (
-        <div style={ {overflowY: 'scroll', border: '1px solid red', height: '500px'} }>
+        <div style={ {overflowY: 'scroll', border: '1px solid ligth-red', height: '500px'} }>
             { children }
         </div>
     );
--- src/container/App.js
+++ src/container/App.js
@@ -29,19 +29,20 @@ class App extends Component {
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter( robot => {
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+
+    const { robots, searchfield } = this.state;
+
+    const filteredRobots = robots.filter( robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
 
-
-    if (this.state.robots.length === 0) {
+    if (!robots.length) {
       return <h1>Loading robots!</h1>;
     } else {
       return (
         <div className="tc">
-          <header className="App-header">
+
             <h1 className="f1">Robofriends</h1>
-          </header>
           <Searchbox searchChange={ this.onSearchChange }/>
           <Scroll>
             <CardList robots={ filteredRobots }/>
